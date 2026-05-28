858bd92cbd9b88066b8101c87222a6ff0377de99
pragya638 <pragyadwivedi415@gmail.com>
2026-05-28 23:08:59 +0530

    Added public home endpoint

---
+++ src/main/java/com/project/fitness/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.project.fitness.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Fitness Tracker Backend API is running successfully.";
+    }
+}
