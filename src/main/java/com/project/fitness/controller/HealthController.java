41d4478ad3d0894aafa15586cfa75b403bc1f337
pragya638 <pragyadwivedi415@gmail.com>
2026-05-30 14:25:23 +0530

    add health and home endpoint

--- src/main/java/com/project/fitness/controller/HealthController.java
+++ src/main/java/com/project/fitness/controller/HealthController.java
@@ -1,26 +1,23 @@
 package com.project.fitness.controller;
 
-import java.util.HashMap;
-import java.util.Map;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+
+import org.springframework.http.ResponseEntity;
 
 
 @RestController
 public class HealthController {
 
     @GetMapping("/")
-    public Map<String, String> home() {
+    public String home() {
+        return "Fitness Tracker Backend API is Running";
+    }
 
-        Map<String, String> response = new HashMap<>();
-
-        response.put("project", "Fitness Tracker Backend API");
-        response.put("status", "Running");
-        response.put("developer", "Pragya Dwivedi");
-        response.put("swagger",
-                "https://fitness-track-backend-vcwk.onrender.com/swagger-ui/index.html");
-
-        return response;
+    @GetMapping("/health")
+    public String health() {
+        return "OK";
     }
 }
